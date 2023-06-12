ad8a4f4869294e015157b66501ffc84c5ec0e56b
EugenKovacec <71874722+EugenKovacec@users.noreply.github.com>
2023-06-12 21:45:08 +0200

    Update dodaj_recept.php

--- dodaj_recept.php
+++ dodaj_recept.php
@@ -1,13 +1,11 @@
 <?php
-// Učitavanje podataka iz POST zahtjeva
+
 $naziv = $_POST['naziv'];
 $sastojci = $_POST['sastojci'];
 $upute = $_POST['upute'];
 $kategorija = $_POST['kategorija'];
 
-// Provjera jesu li svi podaci uneseni
 if ($naziv && $sastojci && $upute && $kategorija) {
-    // Učitavanje postojeće XML datoteke ili stvaranje nove
     $xml = new DOMDocument();
     if (file_exists('recepti.xml')) {
         $xml->load('recepti.xml');
@@ -15,10 +13,8 @@ if ($naziv && $sastojci && $upute && $kategorija) {
         $xml->appendChild($xml->createElement('recepti'));
     }
 
-    // Kreiranje XML elementa za novi recept
     $recept = $xml->createElement('recept');
 
-    // Dodavanje podataka o receptu
     $nazivElement = $xml->createElement('naziv', $naziv);
     $recept->appendChild($nazivElement);
 
@@ -31,14 +27,11 @@ if ($naziv && $sastojci && $upute && $kategorija) {
     $kategorijaElement = $xml->createElement('kategorija', $kategorija);
     $recept->appendChild($kategorijaElement);
 
-    // Dodavanje novog recepta u XML datoteku
     $recepti = $xml->getElementsByTagName('recepti')->item(0);
     $recepti->appendChild($recept);
 
-    // Spremanje promjena u XML datoteku
     $xml->save('recepti.xml');
 
-    // Preusmjeravanje na početnu stranicu
     header('Location: index.php');
     exit;
 } else {
